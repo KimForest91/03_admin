3aaca790fed5c69f2ec33d80401685709464ca97
KimForest91 <forest_91@naver.com>
2023-03-21 13:21:50 +0900

    230321

--- js/common.js
+++ js/common.js
@@ -1,11 +1,14 @@
 // common script
-preventDefaultAnchor();
-// 상단 튐 방지
-function preventDefaultAnchor() {
+$(function() {
+
+  // 상단 튐 방지
   $(document).on('click', 'a[href="#"]', function(e) {
     e.preventDefault();
   });
-}
+
+
+});
+
 
 
 
